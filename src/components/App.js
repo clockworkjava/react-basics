12cfea645e265b21a1efb3bebb34bd783a827efc
Pawel Cwik <clockworkjava01@gmail.com>
2018-07-07 21:05:59 +0200

    First Component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render() {
+        return <div><i>Hello World</i></div>
+    }
+
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
