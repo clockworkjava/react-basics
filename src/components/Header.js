025568d753084718b7893ea95f02d26e2ddec807
Pawel Cwik <clockworkjava01@gmail.com>
2018-07-14 22:28:14 +0200

    Introducing state

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,47 @@ import React from 'react';
 
 class Header extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            bookstoreName : "Black Books",
+            clicked : true,
+            textColor : "white",
+            backgroundColor : "black"
+        }
+    }
+
+    handleClick = () => {
+
+        if(this.state.clicked) {
+            this.setState({
+                bookstoreName : "White Books",
+                textColor : "black",
+                backgroundColor : "white"
+            })
+        } else {
+            this.setState({
+                bookstoreName : "Black Books",
+                textColor : "white",
+                backgroundColor : "black"
+             
+            })
+        }
+        this.setState({
+            clicked : !this.state.clicked
+        })
+    }
+
     render() {
+
+        let headerCss = {
+            color : this.state.textColor,
+            backgroundColor : this.state.backgroundColor
+        }
+
         return (
-            <div className="row header">
-                <center><h1>Header</h1></center>
+            <div className="row header" style={headerCss} onClick={this.handleClick}>
+                <center><h1>{this.state.bookstoreName}</h1></center>
             </div>
         );
     }
