ec27b5818a05e75b5b368e47449620dcd512d480
Pawel Cwik <clockworkjava01@gmail.com>
2018-07-21 22:34:31 +0200

    Add book form added

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -2,9 +2,28 @@ import React from 'react';
 
 class AdminPanel extends React.Component {
     render() {
+
         return (
             <div className="adminPanel col-xs-4">
-                Panel Administratora
+                <form>
+                    <div className="form-group">
+                        <input type="text" placeholder="Book name" id="name" name="name" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" placeholder="Book author" id="author" name="author" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <textarea placeholder="Book description" id="description" name="description" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="checkbox" id="onStock" name="onStock" className="form-check-input"/>
+                        <label for="onStock" className="form-check-label">On stock</label>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" placeholder="Book image" id="image" name="image" className="form-control"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Add</button>
+                </form>
             </div>);
     }
 }
