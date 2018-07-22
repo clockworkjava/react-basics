099219aa1a69c32629f5e1ba0b0866c67345279b
Pawel Cwik <clockworkjava01@gmail.com>
2018-07-22 21:34:21 +0200

    Checkbox fix

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -17,10 +17,19 @@ class AdminPanel extends React.Component {
 
     handleChange = (event) => {
 
-        const newBook = {
-            ...this.state.book,
-            [event.target.name]: event.target.value
-        };
+        let newBook;
+
+        if(event.target.name==="onStock") {
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.checked
+            };
+        } else {
+            newBook = {
+                ...this.state.book,
+                [event.target.name]: event.target.value
+            };
+        }
 
         this.setState({
             book: newBook
@@ -38,21 +47,21 @@ class AdminPanel extends React.Component {
                             onChange={this.handleChange} value={this.state.book.name} />
                     </div>
                     <div className="form-group">
-                        <input type="text" placeholder="Book author" id="author" name="author" className="form-control" 
-                            onChange={this.handleChange} value={this.state.book.author}/>
+                        <input type="text" placeholder="Book author" id="author" name="author" className="form-control"
+                            onChange={this.handleChange} value={this.state.book.author} />
                     </div>
                     <div className="form-group">
-                        <textarea placeholder="Book description" id="description" name="description" className="form-control" 
-                            onChange={this.handleChange} value={this.state.book.description}/>
+                        <textarea placeholder="Book description" id="description" name="description" className="form-control"
+                            onChange={this.handleChange} value={this.state.book.description} />
                     </div>
                     <div className="form-group">
-                        <input type="checkbox" id="onStock" name="onStock" className="form-check-input" 
-                            onChange={this.handleChange} value={this.state.book.onStock}/>
+                        <input type="checkbox" id="onStock" name="onStock" className="form-check-input"
+                            onChange={this.handleChange} value={this.state.book.onStock} />
                         <label htmlFor="onStock" className="form-check-label">On stock</label>
                     </div>
                     <div className="form-group">
-                        <input type="text" placeholder="Book image" id="image" name="image" className="form-control" 
-                            onChange={this.handleChange} value={this.state.book.image}/>
+                        <input type="text" placeholder="Book image" id="image" name="image" className="form-control"
+                            onChange={this.handleChange} value={this.state.book.image} />
                     </div>
                     <button type="submit" className="btn btn-primary">Add</button>
                 </form>
