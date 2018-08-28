24db96a3acbe7a14412ec0fc0e93ed5a6d6ca969
Pawel Cwik <clockworkjava01@gmail.com>
2018-08-28 21:52:51 +0200

    Login form style changes

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -99,13 +99,19 @@ class AdminPanel extends React.Component {
         return (
             <div>
             {!this.state.loggedIn &&
-                <form onSubmit={this.authenticate}>
-                    <input type="text" placeholder="email" id="email_bs" name="email" className="form-control" 
-                        onChange={this.handleLoginChange} value={this.state.email}/>
-                    <input type="password"  id="password_bs" name="password" className="form-control" 
-                        onChange={this.handleLoginChange} value={this.state.password}/>
-                    <button type="submit" className="btn btn-primary">Log in</button>
-                </form>
+                <div className="col-xs-4 col-xs-offset-2 loginForm" >
+                    <form onSubmit={this.authenticate}>
+                        <div className="form-group">
+                            <input type="text" placeholder="email" id="email_bs" name="email" className="form-control" 
+                                onChange={this.handleLoginChange} value={this.state.email}/>
+                        </div>
+                        <div className="form-group">
+                            <input type="password"  id="password_bs" name="password" className="form-control" 
+                                onChange={this.handleLoginChange} value={this.state.password}/>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Log in</button>
+                    </form>
+                </div>
             }
             {this.state.loggedIn && 
                 <div className="adminPanel col-xs-4">
