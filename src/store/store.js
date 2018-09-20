72fb58aafa6aa7485d7d94f7cd34de7407dc0d51
Pawel Cwik <clockworkjava01@gmail.com>
2018-09-20 22:42:55 +0200

    React dev tools

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import {createStore} from 'redux';
 import adminPanelReducer from './adminPanelReducer';
 
-const store = createStore(adminPanelReducer);
+const store = createStore(adminPanelReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
